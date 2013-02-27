921d02ef5ac57dcfe05879c26b5e2a5c52bdede6
Gabriel Morell <gmp@h4xmb.org>
2013-02-27 18:13:54 -0500

    Add returns for the IRC Bot to print'

--- tvctl.py
+++ tvctl.py
@@ -6,30 +6,40 @@ class NEC:
 
   def on(self):
     self.serial.write('\x30\x30\x21\x0D')
+    return "Turned TV On (pls wait 1m before doing this again)"
 
   def off(self):
     self.serial.write('\x30\x30\x21\x0D')
+    return "Turned TV Off (pls wait 1m before doing this again)"
 
   def vid1(self):
     self.serial.write('\x30\x30\x5F\x76\x31\x0D')
+    return "Switched Input to Video1"
 
   def vid2(self):
     self.serial.write('\x30\x30\x5F\x76\x34\x0D')
+    return "Switched Input to Video2"
 
   def svid(self):
     self.serial.write('\x30\x30\x5F\x76\x33\x0D')
+    return "Switched Input to SVideo"
 
   def hd1(self):
     self.serial.write('\x30\x30\x5F\x76\x32\x0D')
+    return "Switched Input to HD1"
 
   def hd2(self):
     self.serial.write('\x30\x30\x5F\x76\x35\x0D')
+    return "Switched Input to HD2"
 
   def vga(self):
     self.serial.write('\x30\x30\x5F\x72\x32\x0D')
+    return "Switched Input to VGA"
 
   def rgb(self):
     self.serial.write('\x30\x30\x5F\x72\x33\x0D')
+    return "Switched Input to Component??"
 
   def dvi(self):
     self.serial.write('\x30\x30\x5F\x72\x31\x0D')
+    return "Switched Input to DVI"
